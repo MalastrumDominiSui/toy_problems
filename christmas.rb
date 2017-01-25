a324eaf67be77ae69d057ce3ea3955a6782231f5
Michael Sefranek <msefran@gmail.com>
2017-01-25 09:41:18 -0600

    added christmas

---
+++ christmas.rb
@@ -0,0 +1,19 @@
+
+def twelveDays(num)
+	daynames = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"]
+	gifts = ["Partridge in a pear tree", "Turtle Doves", "French Hens", "Calling Birds", "Goldeeeeeeen Riiiinggggsss", "Geese a Laying","Geese a Laying", "Maids a Milking", "Ladies Dancing", "Lords a Leaping", "Pipers Piping", "Drummers Drumming"]
+
+	i = 1
+	while i <= num do
+		puts "On the " + daynames[i-1] + " day of Christmas \nmy true love sent to me:"
+		j=i
+			while j>0 do
+			puts "#{j} " + gifts[j-1]
+			j -=1
+			end
+		i +=1
+		puts "\n"
+	end
+end
+
+twelveDays(12)
