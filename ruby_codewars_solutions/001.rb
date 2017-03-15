2f94cba152ad56190988b66b9a280fe32f38cb87
Michael Sefranek <msefran@gmail.com>
2017-03-14 22:23:44 -0500

    longest added

--- ruby_codewars_solutions/001.rb
+++ ruby_codewars_solutions/001.rb
@@ -11,3 +11,13 @@ end
 str = "How can mirrors be real if our eyes aren't real"
 expect = "How Can Mirrors Be Real If Our Eyes Aren't Real"
 puts "toJadenCase is " + (str.toJadenCase == expect).to_s
+
+def longest(a1, a2)
+	a1 = a1.chars.uniq
+	a2 = a2.chars.uniq
+	return (a1 | a2).sort.join
+end
+
+puts "longest test is " + (longest("aretheyhere","yestheyarehere") == "aehrsty").to_s
+puts "longest test 2 is " + (longest("loopingisfunbutdangerous", "lessdangerousthancoding")== "abcdefghilnoprstu").to_s
+puts "longest test 3 is " + (longest("inmanylanguages", "theresapairoffunctions") ==  "acefghilmnoprstuy").to_s
